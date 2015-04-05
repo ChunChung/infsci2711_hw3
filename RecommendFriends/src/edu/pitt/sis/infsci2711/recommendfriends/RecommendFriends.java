dd23045977d7d4a3f5ab37c0fc36c7760bcb283e
Chun-Chung Yang <chunchung.yang@gmail.com>
2015-04-05 13:43:34 -0400

    Update RecommendFriends.java

--- RecommendFriends/src/edu/pitt/sis/infsci2711/recommendfriends/RecommendFriends.java
+++ RecommendFriends/src/edu/pitt/sis/infsci2711/recommendfriends/RecommendFriends.java
@@ -37,96 +37,96 @@ public class RecommendFriends extends Configured implements Tool {
 	}
 
 	@Override
-		public int run(String[] args) throws Exception {
-			System.out.println(Arrays.toString(args));
-			Job job = new Job(getConf(), "RecommendFriends");
-			job.setJarByClass(RecommendFriends.class);
-			job.setMapOutputKeyClass(Text.class);
-			job.setMapOutputValueClass(FriendCountWritable.class);
-			job.setOutputKeyClass(Text.class);
-			job.setOutputValueClass(Text.class);
+	public int run(String[] args) throws Exception {
+		System.out.println(Arrays.toString(args));
+		Job job = new Job(getConf(), "RecommendFriends");
+		job.setJarByClass(RecommendFriends.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(FriendCountWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
 
-			job.setMapperClass(Map.class);
-			job.setReducerClass(Reduce.class);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
 
-			job.setInputFormatClass(TextInputFormat.class);
-			job.setOutputFormatClass(TextOutputFormat.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
-			FileInputFormat.addInputPath(job, new Path(args[0]));
-			FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-			job.waitForCompletion(true);
+		job.waitForCompletion(true);
 
-			return 0;
-		}
+		return 0;
+	}
 
 	public static class Map extends Mapper<LongWritable, Text, Text, FriendCountWritable> {
 
 		private Text word = new Text();
 
 		@Override
-			public void map(LongWritable key, Text value, Context context)
-			throws IOException, InterruptedException {
-			String line[] = value.toString().split("\t");
-			String user = line[0];
-			if(line.length == 2) {
-				for (String friend: line[1].trim().split(",")) {
-					if(friend.isEmpty()) continue;
-					word.set(user);
-					context.write(word, new FriendCountWritable(Long.valueOf(friend),-1L));
-					for (String friend2: line[1].trim().split(",")) {
-						if(!friend.equals(friend2)) {
-							if(friend2.isEmpty()) continue;
-							word.set(friend);
-							context.write(word,  new FriendCountWritable(Long.valueOf(friend2),1L));
-						}
+		public void map(LongWritable key, Text value, Context context)
+		throws IOException, InterruptedException {
+		String line[] = value.toString().split("\t");
+		String user = line[0];
+		if(line.length == 2) {
+			for (String friend: line[1].trim().split(",")) {
+				if(friend.isEmpty()) continue;
+				word.set(user);
+				context.write(word, new FriendCountWritable(Long.valueOf(friend),-1L));
+				for (String friend2: line[1].trim().split(",")) {
+					if(!friend.equals(friend2)) {
+						if(friend2.isEmpty()) continue;
+						word.set(friend);
+						context.write(word,  new FriendCountWritable(Long.valueOf(friend2),1L));
 					}
-				}   	  
-			}
+				}
+			}   	  
+		}
 
-			}
+		}
 	}
 
 	public static class Reduce extends Reducer<Text, FriendCountWritable, Text, Text> {
 		@Override
-			public void reduce(Text key, Iterable<FriendCountWritable> values, Context context)
-			throws IOException, InterruptedException {
-			String recommendations = "";
-			HashMap<Long, Long> recommendFriends = new HashMap<Long, Long>();
-			for (FriendCountWritable val : values) {
-				//sum += val.get();
-				if(recommendFriends.containsKey(val.user)) {
-					if(recommendFriends.get(val.user) == -1L) {
-						continue;
-					}
-					else{
-						recommendFriends.put(val.user, recommendFriends.get(val.user) + val.count);
-					}
+		public void reduce(Text key, Iterable<FriendCountWritable> values, Context context)
+		throws IOException, InterruptedException {
+		String recommendations = "";
+		HashMap<Long, Long> recommendFriends = new HashMap<Long, Long>();
+		for (FriendCountWritable val : values) {
+			//sum += val.get();
+			if(recommendFriends.containsKey(val.user)) {
+				if(recommendFriends.get(val.user) == -1L) {
+					continue;
 				}
 				else{
-					recommendFriends.put(val.user, val.count);
+					recommendFriends.put(val.user, recommendFriends.get(val.user) + val.count);
 				}
 			}
-			ValueComparator bvc =  new ValueComparator(recommendFriends);
-			TreeMap<Long, Long> sortedMap = new TreeMap<Long, Long>(bvc);
-			sortedMap.putAll(recommendFriends);
-			int i = 0;
-			System.out.println(sortedMap);
-			for(java.util.Map.Entry<Long,Long> entry : sortedMap.entrySet()) {
-				Long key_data = entry.getKey();
-				Long value = entry.getValue();
-				if(value==-1L)
-					break;
-				if(recommendations == "")
-					recommendations = String.valueOf(key_data);
-				else
-					recommendations = recommendations + "," + String.valueOf(key_data); 
-				i = i + 1;
-				if(i>=10)
-					break;
+			else{
+				recommendFriends.put(val.user, val.count);
 			}
-			context.write(key, new Text(recommendations));
-			}
+		}
+		ValueComparator bvc =  new ValueComparator(recommendFriends);
+		TreeMap<Long, Long> sortedMap = new TreeMap<Long, Long>(bvc);
+		sortedMap.putAll(recommendFriends);
+		int i = 0;
+		System.out.println(sortedMap);
+		for(java.util.Map.Entry<Long,Long> entry : sortedMap.entrySet()) {
+			Long key_data = entry.getKey();
+			Long value = entry.getValue();
+			if(value==-1L)
+				break;
+			if(recommendations == "")
+				recommendations = String.valueOf(key_data);
+			else
+				recommendations = recommendations + "," + String.valueOf(key_data); 
+			i = i + 1;
+			if(i>=10)
+				break;
+		}
+		context.write(key, new Text(recommendations));
+		}
 	}
 
 	static public class FriendCountWritable implements Writable {
@@ -143,21 +143,21 @@ public class RecommendFriends extends Configured implements Tool {
 		}
 
 		@Override
-			public void write(DataOutput out) throws IOException {
-				out.writeLong(user);
-				out.writeLong(count);
-			}
+		public void write(DataOutput out) throws IOException {
+			out.writeLong(user);
+			out.writeLong(count);
+		}
 
 		@Override
-			public void readFields(DataInput in) throws IOException {
-				user = in.readLong();
-				count = in.readLong();
-			}
+		public void readFields(DataInput in) throws IOException {
+			user = in.readLong();
+			count = in.readLong();
+		}
 
 		@Override
-			public String toString() {
-				return String.valueOf(user)+" "+ String.valueOf(count);
-			}
+		public String toString() {
+			return String.valueOf(user)+" "+ String.valueOf(count);
+		}
 	}
 
 	static class ValueComparator implements Comparator<Long> {
